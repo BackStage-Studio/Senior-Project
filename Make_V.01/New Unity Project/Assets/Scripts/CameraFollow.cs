bdf7af4658a0d0134babe62f21463902484fc201
kittipas klinlek <kittipas.blackisland@gmail.com>
2015-11-07 15:53:50 +0700

    Push_01

---
+++ Make_V.01/New Unity Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+//	public Transform Player;
+//	public float followSpeed = 5.0f;
+//	
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		//transform.position = new Vector3 (Player.position.x, transform.position.y, transform.position.z);
+//		transform.position = Vector3.Lerp(transform.position, Player.position+transform.position, followSpeed);
+//	}
+
+	public bool followCharacter;
+	public bool yFollow;
+	public GameObject player;
+	public float camSpeed;
+	
+	private float fixedSigmaY;
+	
+	void Start(){
+		fixedSigmaY = transform.position.y - player.transform.position.y;
+	}
+	
+	void Update(){
+		if(followCharacter){
+			followCam();
+		}
+	}
+	
+	private void followCam(){
+		float sigmaY = transform.position.y - player.transform.position.y;
+		Vector3 playerPos;
+		if (yFollow) {
+			playerPos = new Vector3(player.transform.position.x,player.transform.position.y+fixedSigmaY,transform.position.z);
+		} else {
+			playerPos = new Vector3(player.transform.position.x,player.transform.position.y+sigmaY,transform.position.z);
+		}
+		transform.position = Vector3.Lerp (transform.position, playerPos, camSpeed);
+	}
+}
---
+++ Make_V.01/New Unity Project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 10.0f;
+	public float g = 10.0f;
+
+	private int mainSpeed = 100;
+	private GameObject PlayerChar ;
+	// Use this for initialization
+	void Start () {
+		//PlayerChar = GetComponent<this.gameObject>();
+	}
+	
+	void FixedUpdate(){
+		//PlayerMove ();
+		Movement ();
+	}
+	void Update () {
+		//PlayerMove ();
+		//Movement ();
+
+	}
+
+	private void Movement()
+	{
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Translate(Vector2.right * walkSpeed *Time.deltaTime);
+			transform.eulerAngles = new Vector2 (0,0);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate(Vector2.right * walkSpeed *Time.deltaTime);
+			transform.eulerAngles = new Vector2 (0,180);
+		}
+	}
+
+
+}
